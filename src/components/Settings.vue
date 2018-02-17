5dc8d883848643c0d95eb48dc03fa41d9e4828ad
Daniel Geymayer <dag0310@gmail.com>
2018-02-17 16:46:06 +0100

    Add option to set the identity on the settings page
    Remove a lot of hardcoded test data noise in the process

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -8,11 +8,24 @@
     </v-ons-toolbar>
     <div class="content">
       <v-ons-list>
+        <v-ons-list-header>Copyright</v-ons-list-header>
         <v-ons-list-item>
           <div class="center">
             © 2018 Vernam, Daniel Geymayer
           </div>
         </v-ons-list-item>
+        <v-ons-list-header>Set identity</v-ons-list-header>
+        <v-ons-list-item>
+          <div class="center">
+            <v-ons-button modifier="large" @click="setIdentity('00436801234567', '123')">Daniel Geymayer</v-ons-button>
+          </div>
+        </v-ons-list-item>
+        <v-ons-list-item>
+          <div class="center">
+            <v-ons-button modifier="large" @click="setIdentity('00436641234567', '456')">John Doe</v-ons-button>
+          </div>
+        </v-ons-list-item>
+        <v-ons-list-header>Danger zone</v-ons-list-header>
         <v-ons-list-item>
           <div class="center">
             <v-ons-button modifier="large" @click="resetAppData()">Reset app data</v-ons-button>
@@ -27,6 +40,11 @@
 export default {
   name: 'settings',
   methods: {
+    setIdentity (id, authToken) {
+      this.$store.commit('setId', id)
+      this.$store.commit('setAuthToken', authToken)
+      window.location.reload()
+    },
     resetAppData () {
       this.$ons.openActionSheet({ buttons: ['Reset app data', 'Cancel'], title: 'This will also delete all your keys.', cancelable: true, destructive: 0 }).then(response => {
         if (response === 0) {
--- src/store.js
+++ src/store.js
@@ -9,6 +9,12 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   plugins: [createPersistedState()],
   mutations: {
+    setId (state, id) {
+      state.id = id
+    },
+    setAuthToken (state, authToken) {
+      state.authToken = authToken
+    },
     setCurrentConversationId (state, id) {
       state.currentConversationId = id
     },
@@ -47,115 +53,22 @@ export default new Vuex.Store({
   state: {
     conversations: [
       {
-        id: '00436801234567',
-        name: 'Daniel Geymayer',
-        messages: [],
-        message: '',
-        newMessages: false,
-        ownKey: btoa(decoder.decode(Uint8Array.from([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]))),
-        otherKey: btoa(decoder.decode(Uint8Array.from([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5])))
-      },
-      {
         id: '00436641234567',
         name: 'John Doe',
-        messages: [
-          {
-            id: 1,
-            own: false,
-            text: 'Hi Daniel!',
-            timestamp: 1517757771000
-          },
-          {
-            id: 2,
-            own: true,
-            text: 'Oh hi, John!',
-            timestamp: 1517757775000
-          },
-          {
-            id: 3,
-            own: false,
-            text: 'We can drive right up to the flat, this is going to be amazing! :)',
-            timestamp: 1517757979000
-          },
-          {
-            id: 4,
-            own: false,
-            text: 'Always blue! Always blue!',
-            timestamp: 1517767979000
-          },
-          {
-            id: 5,
-            own: false,
-            text: 'Always blue! Always blue!',
-            timestamp: 1517777979000
-          },
-          {
-            id: 6,
-            own: false,
-            text: 'Always blue! Always blue!',
-            timestamp: 1517787979000
-          },
-          {
-            id: 7,
-            own: false,
-            text: 'Always blue! Always blue!',
-            timestamp: 1517797979000
-          },
-          {
-            id: 8,
-            own: false,
-            text: 'Always blue! Always blue!',
-            timestamp: 1517807979000
-          },
-          {
-            id: 9,
-            own: false,
-            text: 'Always blue! Always blue!',
-            timestamp: 1517817979000
-          },
-          {
-            id: 10,
-            own: true,
-            text: 'Last!',
-            timestamp: 1517827979000
-          }
-        ],
+        messages: [],
         message: '',
         newMessages: false,
         ownKey: btoa(decoder.decode(Uint8Array.from([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]))),
         otherKey: btoa(decoder.decode(Uint8Array.from([5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1])))
       },
       {
-        id: '00436991234567',
-        name: 'Max Musterman',
-        messages: [
-          {
-            id: 1,
-            own: false,
-            text: 'Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊 Ok 😊',
-            timestamp: 1517324400000
-          }
-        ],
+        id: '00436801234567',
+        name: 'Daniel Geymayer',
+        messages: [],
         message: '',
         newMessages: false,
         ownKey: btoa(decoder.decode(Uint8Array.from([5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1, 5, 4, 3, 2, 1]))),
         otherKey: btoa(decoder.decode(Uint8Array.from([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5])))
-      },
-      {
-        id: '00436001234567',
-        name: 'Aaron A. Aaronson',
-        messages: [
-          {
-            id: 1,
-            own: false,
-            text: 'if you say so, i am not so sure though :/',
-            timestamp: 1517864396599
-          }
-        ],
-        message: '',
-        newMessages: false,
-        ownKey: btoa(decoder.decode(Uint8Array.from([]))),
-        otherKey: btoa(decoder.decode(Uint8Array.from([])))
       }
     ],
     currentConversationId: null,
