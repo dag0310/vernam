23ace4ef8c5a96b5b689005b05a758ba6c4cf7dd
Daniel Geymayer <dag0310@gmail.com>
2018-02-20 21:24:49 +0100

    Refactoring: Set isToday variable differently

--- src/main.js
+++ src/main.js
@@ -24,19 +24,17 @@ Vue.mixin({
       const todayAtMidnightTimestamp = (new Date(now.getFullYear(), now.getMonth(), now.getDate())).getTime()
       const dayInMs = 3600000 * 24
       const timeText = ('' + (date.getHours())).padStart(2, '0') + ':' + ('' + (date.getMinutes())).padStart(2, '0')
-      let isToday, dateText
+      let isToday = false
+      let dateText
 
       if (timestamp >= todayAtMidnightTimestamp) {
         isToday = true
         dateText = 'Today'
       } else if (timestamp >= todayAtMidnightTimestamp - dayInMs) {
-        isToday = false
         dateText = 'Yesterday'
       } else if (timestamp >= todayAtMidnightTimestamp - dayInMs * 6) {
-        isToday = false
         dateText = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()]
       } else {
-        isToday = false
         dateText = date.getFullYear() + '-' +
           ('' + (date.getMonth() + 1)).padStart(2, '0') + '-' +
           ('' + (date.getDate())).padStart(2, '0')
