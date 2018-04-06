b7e5c78ef24a8584b2747739d78d4a386ae31e13
Daniel Geymayer <dag0310@gmail.com>
2018-04-06 14:23:26 +0200

    Allow not specifying a checksum for scanning from external QR sources
    no checksum => '---'

--- src/components/RefillKeyActive.vue
+++ src/components/RefillKeyActive.vue
@@ -58,7 +58,7 @@
           const parsedMetaPrefix = this.parseMetaPrefix(result.text.substr(0, metaPrefixLength))
           const checksum = this.generateChecksumFromPhoneNumbers(this.$store.state.id, this.$store.state.currentConversationId)
 
-          if (checksum !== parsedMetaPrefix.checksum) {
+          if (checksum !== parsedMetaPrefix.checksum && parsedMetaPrefix.checksum !== '---') {
             this.$ons.notification.alert('Your conversations\' phone numbers do not fit together.')
             this.$emit('pop-page')
             return
