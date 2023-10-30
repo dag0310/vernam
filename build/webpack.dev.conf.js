bf654ff88e0324c29a78f72cde4a54ea690e259a
Daniel Geymayer <dag0310@gmail.com>
2023-10-30 22:34:12 +0100

    Allow remotely accessing the dev server via LAN

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -24,6 +24,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     historyApiFallback: true,
     hot: true,
     compress: true,
+    disableHostCheck: true,
     host: HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
