c099a6b524985abacc0668d2e52fc4cf144bdcc9
Daniel Geymayer <dag0310@gmail.com>
2023-11-04 19:56:14 +0100

    Add "You: " in front of messages sent in conversations list

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -23,7 +23,7 @@
           <div class="center">
             <span class="list-item__title ellipsis" :class="{bold: conversation.newMessages}">{{ conversation.name }}</span>
             <span class="list-item__subtitle ellipsis" :class="{bold: conversation.newMessages}">
-              {{ lastMessageText(conversation) }}
+              <i v-if="lastMessageIsOwn(conversation)">You: </i>{{ lastMessageText(conversation) }}
             </span>
           </div>
           <div class="right">
@@ -108,6 +108,10 @@ export default {
     lastMessageText (conversation) {
       const lastMessage = this.lastMessage(conversation)
       return lastMessage ? lastMessage.text : ''
+    },
+    lastMessageIsOwn (conversation) {
+      const lastMessage = this.lastMessage(conversation)
+      return lastMessage ? lastMessage.own : false
     },
     lastMessageDateText (conversation) {
       const lastMessage = this.lastMessage(conversation)
