f136e471ca2d28af2d979ff2dbb95c4f391a0ce1
Daniel Geymayer <dag0310@gmail.com>
2023-11-26 20:27:39 +0100

    Set service worker "notificationclick" event listener to open chat in browser
    https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope/notificationclick_event
    
    https://stackoverflow.com/questions/35100759/serviceworkers-focus-tab-clients-is-empty-on-notificationclick

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-undef */
+const URL = 'https://vernam.xyz'
+
 const FALLBACK_LOCALE = 'en'
 const SUPPORTED_LOCALES = ['en', 'de']
 
@@ -17,6 +20,22 @@ self.addEventListener('push', (event) => {
   event.waitUntil(
     self.registration.showNotification(translations[locale].newMessageTitle, {
       icon: '/static/img/favicon-256x256-rounded.png',
+    })
+  )
+})
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close()
+  event.waitUntil(
+    clients.matchAll({ includeUncontrolled: true, type: 'window' }).then((clients) => {
+      for (const client of clients) {
+        if (client.url.includes(URL) && 'focus' in client) {
+          return client.focus()
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/')
+      }
     })
   )
 })
