4a2509dfba8725ea849f1027cde6ee9bbdfa5067
Daniel Geymayer <dag0310@gmail.com>
2024-02-02 13:44:01 +0100

    Revert "Add proper reload again to settings page"
    This reverts commit 5b4418007f65cc5e872c24410b3df8734ca6913e.
    
    Caused buggy behavior after reload page when swiping back to homepage:
    Settings page could not be accessed anymore.

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -146,7 +146,7 @@ export default defineComponent({
             role: 'destructive',
             handler: () => {
               localStorage.clear()
-              window.location.reload()
+              window.location.href = '/'
             },
           },
           {
@@ -171,7 +171,7 @@ export default defineComponent({
           {
             text: this.$t('reloadApp'),
             handler: () => {
-              window.location.reload()
+              window.location.href = '/'
             },
           },
           {
