27489c992dedc9ffd309cf883e154358530a9bbe
Daniel Geymayer <dag0310@gmail.com>
2024-02-07 15:28:41 +0100

    Consider preamble for bytes left indicator while typing, not after

--- src/views/ChatPage.vue
+++ src/views/ChatPage.vue
@@ -8,7 +8,7 @@
         <ion-title @click="showInformationDialog()" style="cursor: pointer">{{ chat.name }}</ion-title>
         <ion-buttons slot="end">
           <ion-button @click="refillKey()">
-            <span :style="{ color: (otpCryptoResult.bytesLeft < 0) ? 'var(--ion-color-danger)' : 'inherit' }">🔑 {{ keyEmpty ? 0 : otpCryptoResult.isKeyLongEnough ? otpCryptoResultWithoutPreamble.remainingKey.length : otpCryptoResult.bytesLeft }}</span>
+            <span :style="{ color: (otpCryptoResult.bytesLeft < 0) ? 'var(--ion-color-danger)' : 'inherit' }">🔑 {{ keyEmpty ? 0 : (message.trim().length <= 0) ? otpCryptoResultWithoutPreamble.bytesLeft : otpCryptoResult.bytesLeft }}</span>
           </ion-button>
         </ion-buttons>
       </ion-toolbar>
