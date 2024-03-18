5ea9ab158512380929433697ad8925bc0862427e
Daniel Geymayer <dag0310@gmail.com>
2024-03-18 12:33:26 +0100

    In settings show key size per contact, not total key

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -22,7 +22,7 @@
       <h2>{{ $t('showQrCodes') }}</h2>
       <ion-input @ionChange="changeNumericSetting($event, 'setNumQrCodes',     1,   60)" v-model="numQrCodes"     :label="$t('amount')"        label-placement="start" type="number" step="1"  inputmode="numeric" autocomplete="off" required></ion-input>
       <ion-input @ionChange="changeNumericSetting($event, 'setBytesPerQrCode', 1, 1000)" v-model="bytesPerQrCode" :label="$t('dataPerQrCode')" label-placement="start" type="number" step="50" inputmode="numeric" autocomplete="off" required></ion-input>
-      <ion-input :label="$t('totalKeyRefillData')" label-placement="start" readonly :value="totalKeySize + ' ' + $t('bytes')"></ion-input>
+      <ion-input :label="$t('keyRefillData')" label-placement="start" readonly :value="keySize + ' ' + $t('bytes')"></ion-input>
       <ion-button @click="restoreQrCodeDefaults()" v-show="numQrCodes !== defaultNumQrCodes || bytesPerQrCode !== defaultBytesPerQrCode" size="small" color="light" class="ion-margin-top">{{ $t('restoreDefaults') }}</ion-button>
       <!-- ABOUT -->
       <h2 @click="showDebugActions()">{{ $t('about') }}</h2>
@@ -112,9 +112,9 @@ export default defineComponent({
         return this.$store.commit('setLastTimestamp', value)
       },
     },
-    totalKeySize(): number {
-      const total = this.numQrCodes * this.bytesPerQrCode
-      return (total >= 0) ? total : 0
+    keySize(): number {
+      const size = (this.numQrCodes * this.bytesPerQrCode) / 2
+      return (size >= 0) ? size : 0
     },
   },
   methods: {
