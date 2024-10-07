2062f8325ea655d138588e26775c1739487c4be3
Daniel Geymayer <dag0310@gmail.com>
2024-10-07 16:04:06 +0200

    Add new favicons

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,33 @@ export default defineConfig({
         background_color: '#000000',
         theme_color: '#000000',
         lang: undefined,
+        icons: [
+          {
+            "src": "/img/favicon-16x16.png",
+            "sizes": "16x16",
+            "type": "image/png"
+          },
+          {
+            "src": "/img/favicon-32x32.png",
+            "sizes": "32x32",
+            "type": "image/png"
+          },
+          {
+            "src": "/img/favicon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/img/favicon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "/img/favicon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+        ],
       },
     }),
   ],
